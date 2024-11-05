bc9f4274377df348f42c737e59bc79ec883544b5
smenon345 <smenon@cherrycreekschools.org>
2024-11-05 17:44:55 +0000

    finished

--- PriorityQueueLab.java
+++ PriorityQueueLab.java
@@ -1,6 +1,25 @@
+// import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 // import statements
 
 public class PriorityQueueLab {
-  
+    public static void main(String args[]) {
+        PriorityQueue<Integer> pQ = new PriorityQueue<>();
+        Scanner scan;
+        try {
+            File file = new File("RandIntegers.txt");
+            scan = new Scanner(file);
+            while(scan.hasNextLine()) {
+                int value = Integer.parseInt(scan.nextLine());
+                pQ.add(value);
+                // String intVals = scan.nextLine();
+                // System.out.println(intVals);
+            }
+            System.out.println(pQ);
+        }catch(FileNotFoundException e) {
+            System.out.println("There isn't a file being inputted idiot");
+        }
+    }
 }
 
